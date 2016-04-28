d538d47d074ee66ed94dffee3f3086b8f6b5d3cb
Mikael Harju <maindi@kapsi.fi>
2016-04-28 20:40:45 +0300

    Add alltabs, prices to login, refactor code, admin checking

---
+++ servu/prices.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "beer": "1",
+  "longdrink": "1.5",
+  "cider": "1.5",
+  "mixer": "1",
+  "shot": "2"
+};
--- servu/server.js
+++ servu/server.js
@@ -12,6 +12,7 @@ var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
 var User   = require('./app/models/user'); // get our mongoose model
 var Transaction   = require('./app/models/transaction');
+var prices = require('./prices');
 
 // =======================
 // configuration =========
@@ -20,22 +21,24 @@ var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
 mongoose.connect(config.database); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
-// use body parser so we can get info from POST and/or URL parameters
+/////////////////
+// Middlewares //
+/////////////////
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// use morgan to log requests to the console
 app.use(morgan('dev'));
-
-// =======================
-// routes ================
-// =======================
-// basic route
-app.get('/', function(req, res) {
-    res.send('Hello! The API is at http://localhost:' + port + '/api');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+app.use(authUser);
+app.use(checkUserClass);
+app.use('/^(\/api\/admin\/)\w*/', adminCheck);
 
-// API ROUTES -------------------
+/////////////////
+// User routes //
+/////////////////
 app.post('/api/register', function(req, res) {
   console.log(mongoose);
 
@@ -67,30 +70,25 @@ app.post('/api/login', function(req, res){
           console.log(err);
         else {
           var token = jwt.sign(req.body.username, app.get('superSecret'));
-          console.log(token);
-          res.json({
-            success: true,
-            token: token
-          });
+          getTab(req.body.username, function(spike){
+            var prices = getPrices();
+            console.log(spike);
+            res.json({
+              success: true,
+              token: token,
+              tab: spike,
+              prices: prices
+            });
+          })
         }
       });
     }
   });
 });
 
-app.all('*', checkUser);
-
 app.get('/api/tab', function(req, res){
-  Transaction.find({username: req.username}, 'amount', function(err, docs){
-    if(err)
-      console.log(err);
-    else{
-      var spike = 0;
-      docs.forEach(function(args){
-        spike += args.amount;
-      });
-      res.json({amount: spike, success: true});
-    }
+  getTab(req.username, function(spike){
+    res.json({tab: spike, success: true});
   });
 });
 
@@ -116,13 +114,55 @@ app.post('/api/tab', function(req, res){
   }
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.get('/api/alltabs', function(req, res){
+Transaction.aggregate(
+[{
+  $group: {
+    _id: "$username",
+    tab: {
+      $sum: "$amount"
+    }
+  }
+}],
+function(err, result) {
+  if (err)
+    console.log(err);
+  else {
+    res.json(result);
+  }
+})
 });
 
-function checkUser(req, res, next) {
+//////////////////
+// Admin routes //
+//////////////////
+
+
+
+/////////////
+// Helpers //
+/////////////
+function getPrices(){
+  return prices;
+}
+
+function getTab(username, cb){
+  Transaction.find({username: username}, 'amount', function(err, docs){
+    if(err)
+      console.log(err);
+    else{
+      var spike = 0;
+      docs.forEach(function(args){
+        spike += args.amount;
+      });
+      cb(spike);
+    }
+})};
+
+//////////////////////////
+// Middleware functions //
+//////////////////////////
+function authUser(req, res, next) {
   if ( req.path == '/' || req.path == '/api/register' || req.path == '/api/login') return next();
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
   if(token){
@@ -140,6 +180,32 @@ function checkUser(req, res, next) {
       message: 'No token provided'
     });
   }
+}
+
+function checkUserClass(req, res, next) {
+  if(req.username){
+    User.findOne({username: req.username}, 'admin', function(err, docs){
+      if(err)
+        console.log(err);
+      else{
+        req.admin = docs.admin;
+        next();
+      }
+    });
+  }else{
+    next();
+  }
+}
+
+function adminCheck(req, res, next) {
+    if(req.admin){
+      next();
+    } else{
+      return res.status(403).send({
+        success: false,
+        message: "You do not have admin rights."
+      })
+    }
 }
 
 // =======================
