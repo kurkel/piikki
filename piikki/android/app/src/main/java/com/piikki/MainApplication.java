9b95012c7ceb2febc4025a5b8da048e681ed1cb2
Aleksi Asikainen <aleksi.asikainen@aalto.fi>
2017-03-02 01:13:05 +0200

    hups

--- piikki/android/app/src/main/java/com/piikki/MainApplication.java
+++ piikki/android/app/src/main/java/com/piikki/MainApplication.java
@@ -35,8 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
 	  new RCTSplashScreenPackage(),
           new VectorIconsPackage(),
-          new ReactNativePushNotificationPackage(),
-	  new BackgroundJobPackage()
+          new ReactNativePushNotificationPackage()
       );
     }
   };
